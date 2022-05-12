cb11f74a2a4143a65d636cf0fec961a8e8078a99
YangWu1227 <yangwu2020@gmail.com>
2022-05-11 23:07:36 -0400

    feat: add find_missing, case_convert, relocate, and freq_tbls to cleaning_utils module

--- src/my_mltools/exceptions.py
+++ src/my_mltools/exceptions.py
@@ -7,7 +7,25 @@
 from typing import List
 
 # ---------------------------------------------------------------------------- #
-#                       Cleaning utils module exceptions                       #
+#                          AWS utils module exceptions                         #
+# ---------------------------------------------------------------------------- #
+
+# ------------------- Column dtype cannot be inferred error ------------------ #
+
+
+class ColumnDtypeInferError(Exception):
+    """
+    Exception raised when a data frame contains column(s) with dtype(s) that cannot be inferred.
+    """
+
+    def __init__(self, col_nms: List[str]) -> None:
+        self.col_nms = col_nms
+
+    def __str__(self) -> str:
+        return f'The dtypes of the following columns cannot be inferred: {self.col_nms}'
+
+# ---------------------------------------------------------------------------- #
+#                        Data cleaning module exceptions                       #
 # ---------------------------------------------------------------------------- #
 
 # ------------------------------- Keyword error ------------------------------ #
@@ -51,3 +69,38 @@ class InvalidIdentifierError(Exception):
 
     def __str__(self) -> str:
         return f'Columns {self.col_nms} are invalid identifiers'
+
+# ----------------- Columns do not have correct dtypes error ----------------- #
+
+
+class InvalidColumnDtypeError(Exception):
+    """
+    Exception raised when user supplied columns do not have the dtypes that the function expects.
+    """
+
+    def __init__(self, col_nms: List[str], dtype: str) -> None:
+        self.col_nms = col_nms
+        self.dtype = dtype
+
+    def __str__(self) -> str:
+        return f"Columns {self.col_nms} are invalid as dtype '{self.dtype}' is expected"
+
+# ------------------------ Invalid mapping input error ----------------------- #
+
+
+class InvalidMappingKeys(Exception):
+    """
+    Exception raised when user supplied invalid mapping keys to encoding functions.
+    """
+
+    def __str__(self) -> str:
+        return "The argument 'mapping' must be a list of dictionaries with 'col' and 'mapping' as the only two keys"
+
+
+class InvalidMappingValues(Exception):
+    """
+    Exception raised when user supplied invalid mapping values to encoding functions.
+    """
+
+    def __str__(self) -> str:
+        return "The argument 'mapping' must be a list of dictionaries with a string and a dictionary as the only two values"
